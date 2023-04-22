9e6166a158db0f3500e0adef2b1e9024b0e6c11b
Agustin Puig <apuig137@gmail.com>
2023-04-22 12:39:49 -0300

    favicon

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ import "./Contact.css"
 const Contact = () => {
     return (
         <div className="contact">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d26278.66286866667!2d-58.435573450648064!3d-34.5830955153357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sabasto!5e0!3m2!1ses!2sar!4v1682105265274!5m2!1ses!2sar" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0113003544975!2d-58.413564623398585!3d-34.603875757508135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca8c854bd13f%3A0xfc0cf5c054da4aa1!2sAbasto%20Shopping!5e0!3m2!1ses!2sar!4v1682174835395!5m2!1ses!2sar" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <div className="contact-networks">
                 <p>Email: tienda@gmail.com</p>
                 <p>Telefono: +54 1234567898</p>
