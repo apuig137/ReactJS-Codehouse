874ceeb28e4f47b6950ce09049416a87a58d8f0a
Agustin Puig <apuig137@gmail.com>
2023-04-22 11:20:03 -0300

    seo

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,8 +3,8 @@ import "./AboutUs.css"
 const AboutUs = () => {
     return (
         <div className="about-us">
+            <img className="about-logo" src="https://firebasestorage.googleapis.com/v0/b/reactjs-coderhouse-4c952.appspot.com/o/logo-removebg-preview-recortado.png?alt=media&token=98b494e2-43ba-4ce7-a7b3-5d8f4663731e" alt="logo"></img>
             <div className="about-text">
-                <p className="about-title">PG INSUMOS</p>
                 <p>Somos una empresa joven y dinámica que nació con el objetivo de ofrecer productos de calidad y servicios personalizados a los usuarios más exigentes.</p>
                 <p>En nuestra tienda online, encontrará una amplia gama de productos para su ordenador, desde periféricos y accesorios hasta componentes de alta gama. Trabajamos con las mejores marcas del mercado para ofrecer productos de calidad y rendimiento garantizados, siempre al mejor precio.</p>
                 <p>En PG Insumos, nos caracterizamos por ofrecer un servicio al cliente personalizado y adaptado a sus necesidades específicas. Nuestro equipo de profesionales está altamente capacitado para asesorarle en todo momento, brindándole la mejor atención y soluciones a medida.</p>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navb">
-            <p className="brand">PG INSUMOS</p>
+            <img className="img-logo" src="https://firebasestorage.googleapis.com/v0/b/reactjs-coderhouse-4c952.appspot.com/o/logonavbar-removebg-preview.png?alt=media&token=cf5afa42-a76b-4728-a253-73f4b8f15a8a" alt="logo"></img>
             <ul>
                 <li className="item-effect"><Link to={`/`}>Inicio</Link></li>
                 <li className="item-effect"><Link to={`/sobre-nosotros`}>Sobre Nosotros</Link></li>
