23093f0b1d53ef6b049d217efbbbc91b8da24dde
Agustin Puig <apuig137@gmail.com>
2024-05-19 16:54:42 -0300

    new nav photo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navb">
-            <img className="img-logo" src="https://firebasestorage.googleapis.com/v0/b/reactjs-coderhouse-4c952.appspot.com/o/logonavbar-removebg-preview.png?alt=media&token=cf5afa42-a76b-4728-a253-73f4b8f15a8a" alt="logo"></img>
+            <img className="img-logo" src="https://firebasestorage.googleapis.com/v0/b/reactjs-coderhouse-4c952.appspot.com/o/logonavbar-removebg-preview.png?alt=media&token=2ba6a385-a00d-44ee-b1e8-33a618a84368" alt="logo"></img>
             <ul>
                 <li className="item-effect"><Link to={`/`}>Inicio</Link></li>
                 <li className="item-effect"><Link to={`/sobre-nosotros`}>Sobre Nosotros</Link></li>
